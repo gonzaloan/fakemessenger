380e879151040e7d1b10cfbd62b61f26d41b9370
Gonzalo Muñoz <gonzalo.munoz.ext@sodexo.com>
2019-08-05 09:39:43 -0400

    Se agregan components

--- fakemsn/src/app/app.module.ts
+++ fakemsn/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ConversationComponent } from './components/conversation/conversation.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import {RouterModule, Routes} from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'conversation', component: ConversationComponent},
+  {path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ConversationComponent,
+    ProfileComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
